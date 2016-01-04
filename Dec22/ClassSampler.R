f1a8cbd22bf9e464dcfb910f7e5e5b2e644e9ea4
Nick Gotelli <ngotelli@uvm.edu>
2016-01-03 22:23:57 -0500

    Initial commit

---
+++ Dec22/ClassSampler.R
@@ -0,0 +1,24 @@
+# Function for partitioning class groups 
+# Use sample(classNames,1) to randomly choose individual students
+# Nick Gotelli
+# January 2nd, 2015
+
+# Read in UVM class roster and extract list of student names
+classList <- read.csv(file="roster.csv",stringsAsFactors = FALSE)
+classNames <- classList$NAME
+
+# FUNCTION RandomGroups() to randomly assign students to groups
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+RandomGroups <- function(z=classNames,groups=7){
+  
+  # use the gl function with integer division for equal numbers
+  GroupAssigns <- gl(n=groups,k=length(z)%/%groups)
+  
+  # use modulo function to divide remaining individuals evenly among groups
+  GroupAssigns <-c(GroupAssigns,(1:(length(z)%%groups)))
+  
+  # create data frame and shuffle group labels
+  GroupDataFrame <- cbind(classNames,group=sample(GroupAssigns))
+  return(GroupDataFrame)
+}
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
