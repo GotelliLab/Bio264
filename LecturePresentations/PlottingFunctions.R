b700176ba3cd0b7c89af68da8019b9bf6f4649d0
Nick Gotelli <ngotelli@uvm.edu>
2016-02-18 14:48:59 -0500

    Added lecture 8 notes

--- LecturePresentations/PlottingFunctions.R
+++ LecturePresentations/PlottingFunctions.R
@@ -86,7 +86,7 @@ Plot5 <- function(A=1:100, c=seq(1,10,length=8),z=0.26) {
 # Create a matrix to hold the data
 # i rows, with one row for each value of A
 # j columns, one for each parameter combinatino of z and c
-  m <- matrix(nrow=length(A),ncol=length(c)*length(z))
+  m <- matrix(nrow=length(A),ncol=length(c))
   
 # Fill the columns of the matrix for the curve
    v <- 1
@@ -103,10 +103,6 @@ Plot5 <- function(A=1:100, c=seq(1,10,length=8),z=0.26) {
          main=paste("c = ",min(c),"-",max(c),
                     "\n",
                     "z = ",z))
-  legend(x="bottomright", 
-         legend= c(paste("c =",min(c),"-",
-                         max(c),"\n", "z =",z)),
-                         bty="n")
 }
 Plot5()
 Plot5(A=1:1000,c=c(3,3.5,10),z=0.11)
@@ -122,8 +118,40 @@ for (x in 1:2) {
 }
 par(opar)
   
-
+# Add some control over the y axis range
+Plot5a <- function(A=1:100, c=seq(1,10,length=8),z=0.26,myY=c(0,100)) {
+  c <- round(c,2)
+  z <- round(z,2)
+  
+# Create a matrix to hold the data
+# i rows, with one row for each value of A
+# j columns, one for each parameter combinatino of z and c
+  m <- matrix(nrow=length(A),ncol=length(c))
+  
+# Fill the columns of the matrix for the curve
+   v <- 1
+  for (i in 1:length(c)) {
+    S <- c[i]*A^z
+    m[,i] <- S
+  }
+ matplot(x=A,y=m,type="l",
+         xlab="Area",
+         ylab="Number of Species",
+         col="black",
+         lty=1,
+         ylim=myY, # insert the ylimit here
+         main=paste("c = ",min(c),"-",max(c),
+                    "\n",
+                    "z = ",z))
+}
+par(mfrow=c(2,2))
+myZ <- c(0.12, 0.14, 0.20, 0.26)
+for (i in 1:length(myZ)){
+  Plot5a(A=1:1000,c=seq(1,10,length=6),z=myZ[i],myY=c(0,62))
+}
+par(opar)
 # Redo function to create all parameter combinations first
+
 
 Plot6 <- function(A=1:100, c=seq(1,10,length=8),z=c(0.15,0.26)) {
   c <- round(c,2)
@@ -153,9 +181,6 @@ Plot6 <- function(A=1:100, c=seq(1,10,length=8),z=c(0.15,0.26)) {
          main=paste("c = ",min(c),"-",max(c),
                     "\n",
                     "z = ",min(z),"-",max(z)))
- # legend(x="bottomright", 
-#          legend= c(paste("c =",min(c),"-",
-#                          max(c),"\n", "z = ",min(z),"-",max(z),bty="n")))
                          
 }
 Plot6()
