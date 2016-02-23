bbb58c60008eb6b56f43d65ab7e37afe92664d32
ngotelli <ngotelli@uvm.edu>
2016-02-23 08:09:52 -0500

    Started Growth Dynamics File

---
+++ GrowthDynamics.R
@@ -0,0 +1,86 @@
+# Modeling differential growth equations
+# February 22, 2016
+# NJG
+
+# basic equation for change plus current value
+# illustrated with familiar exponential growth
+
+rm(list=ls())
+set.seed(100)
+opar <- par(no.readonly=TRUE)
+
+# basic growth model
+#--------------------------------------------------
+Model1 <- function(N_0=250,Time=100,r=0.08){
+  Nvec <- rep(0,Time)
+  Nvec[1] <- N_0
+  for (i in 2:Time) {
+    Nvec[i] <- Nvec[i-1] + r*Nvec[i-1]
+  }
+  return(Nvec)
+  
+}
+Nvec <- Model1()
+plot(x=1:length(Nvec),y=Nvec)
+#--------------------------------------------------
+
+# sensitivity to time-step
+
+r <- 0.08
+BigStep <- seq(1,50,by=1)
+SmallStep <-seq(1,50,by=0.1)
+BigVec <- rep(0,length(BigStep))
+SmallVec <- rep(0,length(SmallStep))
+BigVec[1] <- 250
+SmallVec[1] <- 250
+for (i in 2:length(BigVec)) BigVec[i] <- BigVec[i-1] + r*BigVec[i-1]
+for (i in 2:length(SmallVec)) SmallVec[i] <- SmallVec[i-1] + r*SmallVec[i-1]*0.1 
+tail(BigVec)
+tail(SmallVec)
+250*exp(50*0.08)
+#--------------------------------------------------
+
+# Calculating N, dNdt, (1/N)(dNdt)
+#--------------------------------------------------
+Model2 <- function(N_0=250,Time=100,r=0.08){
+  Nvec <- rep(0,Time)
+  Nvec[1] <- N_0
+  for (i in 2:Time) {
+    Nvec[i] <- Nvec[i-1] + r*Nvec[i-1]
+  }
+    dNVec <- diff(Nvec)
+    dNVec <- c(dNVec,dNVec[length(dNVec)]
+    perCapVec <- dNVec/NVec           
+#--------------------------------------------------
+Model1 <- function(N_0=250,Time=100,r=0.08){
+  Nvec <- rep(0,Time)
+  Nvec[1] <- N_0
+  for (i in 2:Time) {
+    Nvec[i] <- Nvec[i-1] + r*Nvec[i-1]
+  }
+  return(Nvec)
+  
+}
+Nvec <- Model1()
+plot(x=1:length(Nvec),y=Nvec)
+#--------------------------------------------------
+#--------------------------------------------------
+Model1 <- function(N_0=250,Time=100,r=0.08){
+  Nvec <- rep(0,Time)
+  Nvec[1] <- N_0
+  for (i in 2:Time) {
+    Nvec[i] <- Nvec[i-1] + r*Nvec[i-1]
+  }
+  return(Nvec)
+  
+}
+Nvec <- Model1()
+plot(x=1:length(Nvec),y=Nvec)
+#--------------------------------------------------
+  return(Nvec)
+  
+}
+Nvec <- Model1()
+plot(x=1:length(Nvec),y=Nvec)
+#--------------------------------------------------
+
